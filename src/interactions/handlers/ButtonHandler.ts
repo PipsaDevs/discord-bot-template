1df87f0e0df994480ddb739bb8a9ccd3e5ec4739
CyRstudent <hsmad2022@gmail.com>
2026-05-19 10:58:50 +0200

    feat: Finish ButtonHandler handler

--- src/interactions/handlers/ButtonHandler.ts
+++ src/interactions/handlers/ButtonHandler.ts
@@ -1,13 +1,32 @@
 import { type ButtonInteraction, InteractionType } from 'discord.js';
 import type { InteractionHandler } from '../../interfaces/InteractionHandler.js';
+import ButtonOPCodes from '../../enums/ButtonOPCodes.js';
 
 const buttonHandler: InteractionHandler<ButtonInteraction> = {
 	interactionType: InteractionType.MessageComponent,
-	async execute(interaction) {
+	async execute(interaction, client) {
 		if (!interaction.isButton()) {
 			return;
 		}
-		// More to come...
+		const args: string[] = interaction.customId.split('-');
+		if (args[0] === '0') {
+			return;
+		}
+		const opc = ButtonOPCodes[args[0] as keyof typeof ButtonOPCodes];
+		const cmd = client.buttonCommands.get(opc);
+		if (!cmd) {
+			client.emit(
+				'error',
+				new Error(
+					`OPCode ${args[0]} was not found inside Client#buttonCommands collection`,
+				),
+			);
+			await interaction.reply(
+				'¡Ups! Todavía no tengo un programa para ejecutar este botón. Considera reportar este incidente a mi desarrollador.',
+			);
+			return;
+		}
+		await cmd.execute(interaction, client, ...args);
 	},
 };
 

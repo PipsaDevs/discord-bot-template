877fce8027ad5e567f7bddb68360c3d5499608b1
CyRstudent <hsmad2022@gmail.com>
2026-05-19 10:40:48 +0200

    feat: Button OPCodes enum

---
+++ src/enums/ButtonOPCodes.ts
@@ -0,0 +1,10 @@
+enum ButtonOPCodes {
+	ignoreAction = '0',
+	giveRole = '1',
+	/**
+	 * IMPORTANT NOTE:
+	 * When adding a Button OP Code, please make sure to write down how to split arguments and such
+	 */
+}
+
+export default ButtonOPCodes;
